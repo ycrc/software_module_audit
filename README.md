db971d02d875ee20210774ad081a0418cbfa240f
Ben Evans <benjamin.robert.evans@gmail.com>
2021-01-22 13:07:11 -0500

    updated readme, little cleanup

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Software Module Audit
 
+## Running
 
+First, get a tsv with module names and load counts as the first two columns, store in FY directory for that year. Files for each cluster should start with cluster name. Then run
+
+``` bash
+# e.g. FY21
+./mod_audit.py FY21/*.tsv
+```
+
+This will create three files for each cluster.
+
+| Filename               | Function                                               | Notes                                                      |
+|------------------------|--------------------------------------------------------|------------------------------------------------------------|
+| `_admin.list`          | Sets module load messages notifying of deprecation     | Append to `apps/modules/admin.list`                        |
+| `_modulerc.lua`        | Hides deprecated/unused modules from `module avail`    | Append to `apps/modulerc.lua`                              |
+| `_popular_modules.txt` | List of newly deprecated modules that are often loaded | Consider updating/installing these in supported toolchains |
--- mod_audit.py
+++ mod_audit.py
@@ -2,43 +2,42 @@
 from __future__ import print_function
 import sys
 
-
+# Grace/Milgram
 twentyeighteena = ["2018a", "GCCcore-6", "GCC-6"]
 
-# keep these around
+# keep these around everywhere
 univ_curr_toolchains = ["2018b", "2020b", "GCCcore-7", "GCC-7"]
 
 # deprecate these
 univ_depr_toolchains = [
+    "2016.3.210",  # iccifort
     "foss-2016b",
-    "foss/2016b",
-    "gompi/2016b",
-    "gompi-2016b",
-    "intel/2016b",
-    "intel-2016b",
-    "iimpi/2016b",
-    "iimpi-2016b",
-    "2016.3.210", #iccifort
     "foss-2017",
+    "foss/2016b",
     "foss/2017",
     "GCC-4",
-    "GCCcore-4",
     "GCC-5",
+    "GCCcore-4",
     "GCCcore-5",
     "gcccuda-2016",
     "gcccuda-2017",
+    "gompi-2016b",
+    "gompi/2016b",
+    "iimpi-2016b",
+    "iimpi-2017",
+    "iimpi/2016b",
+    "iimpi/2017",
+    "intel-2016b",
     "intel-2017",
+    "intel/2016b",
     "intel/2017",
-    "iompi-2017",
-    "iompi/2017",
     "iomkl-2017",
     "iomkl/2017",
-    "iimpi-2017",
-    "iimpi/2017",
+    "iompi-2017",
+    "iompi/2017",
 ]
 
 # these should already be gone
-# I say we just delete/move the Singularity modules
 prev_depr_toolchains = ["foss-2016a", "foss/2016a", "Singularity"]
 
 # what to say about this round of deprecation
@@ -46,6 +45,7 @@ depr_str = "{}: This module is being deprecated and will be removed from the mod
 
 # modules with loads under load_thresh get hidden
 load_thresh = 10
+
 # except if they match unused_exceptions
 unused_exceptions = [
     "Autoconf/",
@@ -55,27 +55,28 @@ unused_exceptions = [
     "cuDNN/",
     "dSQ/",
     "EasyBuild/",
-    "GCCcore/",
     "GCC/",
+    "GCCcore/",
     "MATLAB/",
-    "ycga-public/",
     "mpifileutils/",
     "TotalView/",
+    "ycga-public/",
 ]
 
+# EasyBuild toolchain dependencies we can hide 
 build_deps = [
-    "Bison/",
     "binutils/",
-    "M4/",
-    "zlib/",
-    "libtool/",
+    "Bison/",
     "bzip2/",
-    "ncurses/",
-    "pkg-config/",
+    "CMake/",
     "flex/",
     "gettext/",
     "help2man/",
-    "CMake/",
+    "libtool/",
+    "M4/",
+    "ncurses/",
+    "pkg-config/",
+    "zlib/",
 ]
 
 # deprecated modules with loads over lots_of_loads get added to pre_builds and saved to popular_modules.txt
@@ -86,11 +87,11 @@ for in_tsv in in_tsvs:
     prefix, _ = in_tsv.split("_", 1)
 
     if prefix in ["Grace", "Milgram"]:
-        curr_toolchains = univ_curr_toolchains+twentyeighteena
+        curr_toolchains = univ_curr_toolchains + twentyeighteena
         depr_toolchains = univ_depr_toolchains
     else:
         curr_toolchains = univ_curr_toolchains
-        depr_toolchains = univ_depr_toolchains+twentyeighteena
+        depr_toolchains = univ_depr_toolchains + twentyeighteena
 
     prev_depr_mods = []
     depr_unused_mods = []
@@ -109,12 +110,12 @@ for in_tsv in in_tsvs:
 
             # hide previously deprecated
             if any(toolchain in mod_str for toolchain in prev_depr_toolchains):
-                prev_depr_mods.append(mod_str)  
+                prev_depr_mods.append(mod_str)
 
             # deprecate with load message
             elif any(toolchain in mod_str for toolchain in depr_toolchains):
                 if loads > lots_of_loads:
-                    pre_builds.add("{}, {}".format(mod_str,loads))
+                    pre_builds.add("{}, {}".format(mod_str, loads))
                 if loads < load_thresh and not any(
                     excepted in mod_str for excepted in unused_exceptions
                 ):
@@ -129,7 +130,7 @@ for in_tsv in in_tsvs:
                     if any(excepted in mod_str for excepted in build_deps):
                         build_deps_mods.append(mod_str)
                     else:
-                        recent_unused_mods.append(mod_str)                    
+                        recent_unused_mods.append(mod_str)
 
             # hide modules no one uses :-(
             elif loads < load_thresh and not any(
@@ -138,25 +139,25 @@ for in_tsv in in_tsvs:
                 if any(excepted in mod_str for excepted in build_deps):
                     build_deps_mods.append(mod_str)
                 else:
-                    other_unused_mods.append(mod_str) 
+                    other_unused_mods.append(mod_str)
 
-        for mod_str in prev_depr_mods:
-            print('hide_version("{}") -- past_deprecated'.format(mod_str), file=mod_rc)
-
-        for mod_str in depr_unused_mods:
-            print('hide_version("{}") -- deprecating_and_unused'.format(mod_str), file=mod_rc)
-
-        for mod_str in other_unused_mods:
-            print('hide_version("{}") -- system_unused'.format(mod_str), file=mod_rc)
-
-        for mod_str in recent_unused_mods:
-            print(
-                'hide_version("{}") -- current_unused'.format(mod_str),
-                file=mod_rc,
-            )
-
-        for mod_str in build_deps_mods:
-            print('hide_version("{}") -- build_dependency'.format(mod_str), file=mod_rc)
+        for mod_str, reason in zip(
+            [
+                prev_depr_mods,
+                depr_unused_mods,
+                other_unused_mods,
+                recent_unused_mods,
+                build_deps_mods,
+            ],
+            [
+                "past_deprecated",
+                "deprecating_and_unused",
+                "system_unused",
+                "current_unused",
+                "build_dependency",
+            ],
+        ):
+            print('hide_version("{}") -- {}'.format(mod_str, reason), file=mod_rc)
 
     with open("{}_popular_modules.txt".format(prefix), "w") as pop_mods:
         print(
